17633cd959a80a3cb1082775e3b3a66ba791624d
judibec <juanchopolo6954@gmail.com>
2023-01-31 00:35:23 -0500

    funciona la API

---
+++ src/main/java/edu/escuelaing/arem/ASE/app/HttpServer.java
@@ -0,0 +1,92 @@
+package edu.escuelaing.arem.ASE.app;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import java.net.*;
+import java.io.*;
+import java.util.Objects;
+
+public class HttpServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(34000);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 34000.");
+            System.exit(1);
+        }
+
+        boolean running = true;
+        while(running) {
+            Socket clientSocket = null;
+            try {
+                System.out.println("Listo para recibir ...");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine, outputLine;
+            String title = "";
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("Received: " + inputLine);
+                if(inputLine.contains("title?name")){
+                    String[] firstSplit = inputLine.split("=");
+                    title = (firstSplit[1].split("HTTP"))[0];
+                }
+                if (!in.ready()) {
+                    break;
+                }
+            }
+            if(!Objects.equals(title, "")){
+                outputLine = "HTTP/1.1 200 OK\r\n"
+                        + "Content-Type: application/json\r\n"
+                        + "\r\n"+
+                        HttpConnection.RsponseApi(title);
+                System.out.println(title);
+            }else {
+                outputLine = "HTTP/1.1 200 OK\r\n"
+                        + "Content-Type: text/html\r\n"
+                        + "\r\n"
+                        + "<!DOCTYPE html>\n" +
+                        "<html>\n" +
+                        "    <head>\n" +
+                        "        <title>Form Example</title>\n" +
+                        "        <meta charset=\"UTF-8\">\n" +
+                        "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                        "    </head>\n" +
+                        "    <body>\n" +
+                        "        <h1>Search your movie GET</h1>\n" +
+                        "        <form action=\"/hello\">\n" +
+                        "            <label for=\"name\">Title:</label><br>\n" +
+                        "            <input type=\"text\" id=\"name\" name=\"name\" value=\"John\"><br><br>\n" +
+                        "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsg()\">\n" +
+                        "        </form> \n" +
+                        "        <div id=\"getrespmsg\"></div>\n" +
+                        "\n" +
+                        "        <script>\n" +
+                        "            function loadGetMsg() {\n" +
+                        "                let nameVar = document.getElementById(\"name\").value;\n" +
+                        "                const xhttp = new XMLHttpRequest();\n" +
+                        "                xhttp.onload = function() {\n" +
+                        "                    document.getElementById(\"getrespmsg\").innerHTML =\n" +
+                        "                    this.responseText;\n" +
+                        "                }\n" +
+                        "                xhttp.open(\"GET\", \"/title?name=\"+nameVar);\n" +
+                        "                xhttp.send();\n" +
+                        "            }\n" +
+                        "        </script>\n" +
+                        "\n" +
+                        "</html>";
+            }
+            out.println(outputLine);
+            out.close();
+            in.close();
+            clientSocket.close();
+        }
+        serverSocket.close();
+    }
+}
